1afb52b12c910632f3722ed90523bc117240769d
Vinicius Longaray <vmouralongaray@gmail.com>
2021-09-08 15:18:06 -0300

    Add support for new chid in table component

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import Divider from "@material-ui/core/Divider";
 
 import AppBar from "../scripts/ui/popup/appbar";
-import ChidTable from "./ui/popup/table";
+import ChidTable from "../scripts/ui/popup/table";
 
 export default function Popup() {
   return (
--- app/scripts/ui/popup/table.jsx
+++ app/scripts/ui/popup/table.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useCallback, useRef, useState, useMemo } from "react";
 import differenceBy from "lodash/differenceBy";
 
+// Components
 import Card from "@material-ui/core/Card";
 import DataTable from "react-data-table-component";
 import Grid from "@material-ui/core/Grid";
@@ -8,14 +9,22 @@ import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
 
+// Icons
 import Add from "@material-ui/icons/AddTwoTone";
 import Edit from "@material-ui/icons/EditTwoTone";
 import Delete from "@material-ui/icons/DeleteTwoTone";
 import Info from "@material-ui/icons/InfoTwoTone";
 
+import Done from "@material-ui/icons/DoneTwoTone";
+import Clear from "@material-ui/icons/ClearTwoTone";
+
 import CustomTablePagination from "../popup/tablePagination";
 
 import mockChanges from "../popup/mockData";
+
+/* -------------------------------------------------------------------------- */
+/*                         Custom components for table                        */
+/* -------------------------------------------------------------------------- */
 
 const title = (
   <Typography variant="overline" style={{ fontSize: 14, fontWeight: 600 }}>
@@ -64,84 +73,255 @@ const actions = (addHandler, editHandler) => (
 );
 
 const contextActions = (deleteHandler) => (
-  // TODO: add cancel button for toggling selection mode
   <IconButton color="secondary" onClick={deleteHandler}>
     <Delete />
   </IconButton>
 );
 
-const columnTitleStyle = (title) => (
+const columnTitle = (title) => (
   <Typography variant="body2" style={{ fontSize: 14, fontWeight: 500 }}>
     {title}
   </Typography>
 );
 
+const EditableCell = ({ row, index, column, col, onChange }) => {
+  const [value, setValue] = useState(column.selector(row));
+
+  const handleOnChange = (e) => {
+    // remove leading zero and dot from input value
+    const name = e.target.name;
+    const value = String(e.target.value).replace(/\D|^0+/, "");
+    setValue(value);
+    onChange?.(name, value);
+  };
+
+  if (column?.editing) {
+    return (
+      <input
+        type={typeof column.selector(row) || "text"}
+        name={column.selector(row)}
+        style={{ width: "100%" }}
+        onChange={handleOnChange}
+        value={value}
+      />
+    );
+  }
+
+  if (col.cell) {
+    return col.cell(row, index, column);
+  }
+  return column.selector(row);
+};
+
+/* -------------------------------------------------------------------------- */
+/*                               Default columns                              */
+/* -------------------------------------------------------------------------- */
+
 const columns = [
   {
-    name: columnTitleStyle("Change-Id"),
+    name: columnTitle("Change-Id"),
     selector: (row) => row.id,
+    editable: true,
   },
   {
-    name: columnTitleStyle("Code-Review"),
+    name: columnTitle("Code-Review"),
     selector: (row) => row.codeReview,
     right: true,
   },
   {
-    name: columnTitleStyle("Verified"),
+    name: columnTitle("Verified"),
     selector: (row) => row.verified,
     right: true,
   },
 ];
 
-export default function ChidTable() {
-  const [toggleSelectable, setToggleSelectable] = React.useState(false);
-  const [toggleCleared, setToggleCleared] = React.useState(false);
+/* -------------------------------------------------------------------------- */
+/*                            Table for Change-Ids                            */
+/* -------------------------------------------------------------------------- */
 
+function ChidTable() {
+  const [paginationPage] = useState(5);
+
+  const [state, setState] = useState({
+    editingId: "",
+    toggleSelection: false,
+    toggleCleared: false,
+  });
+
+  const [data, setData] = useState(mockChanges);
   const [selectedRows, setSelectedRows] = React.useState([]);
 
-  const [data, setData] = React.useState(mockChanges);
+  /* ------------------------------------------------------------------------ */
+  /*                           Handlers for add mode                          */
+  /* ------------------------------------------------------------------------ */
 
-  const handleRowSelected = React.useCallback((state) => {
+  const addMode = () => {
+    if (state.editingId === "" && !state.toggleSelection) {
+      const element = { id: 0, codeReview: 0, verified: 0 };
+      setState({
+        ...state,
+        editingId: 0,
+      });
+      setData([element, ...data]);
+    }
+  };
+
+  const save = (item) => {
+    setState({
+      ...state,
+      editingId: "",
+    });
+
+    const newValue = formData[item.id];
+    if (newValue == undefined) {
+      // input didn't receive any event, just remove tmp entry from data
+      data.shift();
+    } else {
+      const exists = data.find((change) => change.id === newValue);
+      if (exists != undefined) {
+        // change-id already exists, just remove tmp entry from data
+        // TODO: maybe show some notification
+        data.shift();
+      } else {
+        data[0].id = newValue;
+      }
+    }
+
+    setData([...data]);
+  };
+
+  const cancel = () => {
+    setState({
+      ...state,
+      editingId: "",
+    });
+
+    // remove first entry (with id == 0)
+    data.shift();
+    setData([...data]);
+  };
+
+  /* ------------------------------------------------------------------------ */
+  /*                        Handlers for selection mode                       */
+  /* ------------------------------------------------------------------------ */
+
+  const selectMode = () => {
+    if (state.editingId != "") {
+      setState({ ...state, toggleSelection: !state.toggleSelection });
+    }
+  };
+
+  const handleRowSelected = useCallback((state) => {
     setSelectedRows(state.selectedRows);
   }, []);
 
-  const addMode = () => {
-    console.log("uhul");
-  };
-
-  const editMode = () => {
-    setToggleSelectable(!toggleSelectable);
-  };
-
   const deleteAll = () => {
-    const rows = selectedRows.map((r) => r.changeId);
+    const rows = selectedRows.map((r) => r.id);
     // eslint-disable-next-line no-alert
     if (
       window.confirm(`Are you sure you want to delete these entries?\r ${rows}`)
     ) {
-      setToggleCleared(!toggleCleared);
-      setData(differenceBy(data, selectedRows, "changeId"));
+      setState({
+        ...state,
+        toggleCleared: !state.toggleCleared,
+      });
+      setData(differenceBy(data, selectedRows, "id"));
     }
   };
+
+  /* ------------------------------------------------------------------------ */
+  /*            create handler to update input field on change events         */
+  /* ------------------------------------------------------------------------ */
+
+  const isEditing = (record) => record.id === state.editingId;
+  let formData = useRef({}).current;
+
+  const formOnChange = (nam, val) => {
+    formData = {
+      ...formData,
+      [nam]: val,
+    };
+  };
+
+  /* ------------------------------------------------------------------------ */
+  /*         create actions and input field for 'editable' column/row         */
+  /* ------------------------------------------------------------------------ */
+
+  const createColumns = useMemo(() => {
+    const rowActions = [
+      {
+        name: columnTitle("Actions"),
+        allowOverflow: false,
+        width: "80px",
+        cell: (row) => {
+          const editable = isEditing(row);
+          if (editable) {
+            return (
+              <div style={{ width: "100%" }}>
+                <IconButton
+                  color="primary"
+                  size="small"
+                  onClick={() => save(row)}
+                >
+                  <Done fontSize="inherit" />
+                </IconButton>
+                <IconButton color="secondary" size="small" onClick={cancel}>
+                  <Clear fontSize="inherit" />
+                </IconButton>
+              </div>
+            );
+          }
+        },
+      },
+    ];
+
+    return [
+      ...(state.editingId !== "" ? rowActions : []),
+      ...columns.map((col) => {
+        if (!col.editable) {
+          return col;
+        }
+        return {
+          ...col,
+          cell: (row, index, column) => {
+            const editing = isEditing(row);
+            return (
+              <EditableCell
+                row={row}
+                index={index}
+                column={{ ...column, editing }}
+                col={col}
+                onChange={formOnChange}
+              />
+            );
+          },
+        };
+      }),
+    ];
+  }, [data, state.editingId]);
+
+  const { toggleSelection, toggleCleared } = state;
 
   return (
     <Card variant="outlined" style={{ marginTop: 10, height: "100%" }}>
       <DataTable
         title={title}
-        columns={columns}
+        columns={createColumns}
         data={data}
         noDataComponent={emptyData}
-        actions={actions(addMode, editMode)}
+        actions={actions(addMode, selectMode)}
         contextActions={contextActions(deleteAll)}
-        selectableRows={toggleSelectable}
-        onSelectedRowsChange={handleRowSelected}
+        selectableRows={toggleSelection}
         clearSelectedRows={toggleCleared}
+        onSelectedRowsChange={handleRowSelected}
         dense
         highlightOnHover
         pagination
-        paginationPerPage={5}
+        paginationPerPage={paginationPage}
         paginationComponent={CustomTablePagination}
       />
     </Card>
   );
 }
+
+export default ChidTable;
