815084d436fd696a1ac301a64add3a950bdd86b9
Vinicius Longaray <vmouralongaray@gmail.com>
2021-09-14 23:54:48 -0300

    Add axios and API for adding changes

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -1,4 +1,8 @@
 /* Messages types for communication between background service and popup/option */
-const types = { GET_DATA: "get_data", UPDATE_DATA: "update_data" };
+const types = {
+  GET_DATA: "get_data",
+  UPDATE_DATA: "update_data",
+  ADD_CHANGE: "add_change",
+};
 
 export default types;
